ef649a32558f5dfd1099af8c2d9e25bda6c63025
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-06-24 20:46:41 -0400

    working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.io.InvalidClassException;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please tell me your name");
+        String name = scan.nextLine();
+        //System.out.println(name);
+        if (name.equals("Alice")) {
+            System.out.println("Hello " + name + "!");
+        } else if (name.equals("alice")) {
+            System.out.println("Hello " + name + "!");
+        } else if (name.equals("Bob")) {
+            System.out.println("Hello " + name + "!");
+        } else if (name.equals("bob")) {
+            System.out.println("Hello " + name + "!");
+        } else {
+            System.out.println("Stranger danger!");
+        }
     }
 }
